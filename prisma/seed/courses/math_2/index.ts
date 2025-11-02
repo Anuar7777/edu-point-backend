3040448c2df4a0334016b8260834ab956c5b165a
Anuar7777 <aman_anuar@mail.ru>
2025-11-02 17:11:24 +0500

    fix: fix course's image

--- prisma/seed/courses/math_2/index.ts
+++ prisma/seed/courses/math_2/index.ts
@@ -4,7 +4,7 @@ export const course = {
 	courseId: '4126401b-e3e8-4317-8e01-57aab63ce173',
 	title: 'Математика Третий Курс',
 	description: 'Базовый курс по математике для школьников',
-	imageUrl: '/public/images/courses/4126401b-e3e8-4317-8e01-57aab63ce173',
+	imageUrl: '/public/images/courses/4126401b-e3e8-4317-8e01-57aab63ce173.png',
 	sections: sections,
 	totalSections: sections.length,
 }
