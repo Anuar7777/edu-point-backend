27e2d5c94b71d2a10c6610ae83c27f3bf5162856
Anuar7777 <aman_anuar@mail.ru>
2025-11-20 10:24:44 +0500

    refactor: change default value of isBlocked into user-application logic

---
+++ prisma/migrations/20251120051125_change_default_value_of_is_blocked_into_user_application/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "user_application" ALTER COLUMN "is_blocked" SET DEFAULT true;
--- src/user/user-application/user-appllication.service.ts
+++ src/user/user-application/user-appllication.service.ts
@@ -46,6 +46,8 @@ export class UserApplicationService {
 				})
 			}
 		})
+
+		return this.getByUserId(userId)
 	}
 
 	async updateBlockedStatus(
